575de8d25673538d734bb38fc4a55601515f894b
AmauryS08 <57544845+AmauryS08@users.noreply.github.com>
2019-12-22 19:41:03 +0100

    Add files via upload

--- SR01/TP2/exercice3/main.c
+++ SR01/TP2/exercice3/main.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "manipulationMatrice.h"
+#include <unistd.h>
+#define MAX 255
+
+int **creationMatrice(int nbLignes, int nbColonnes);
+int **generationMatriceAleatoire(int nbLignes, int nbColonnes, int valeurMin, int valeurMax);
+void afficherMatrice(int **matrice, int nbLignes, int nbColonnes);
+void affichageSelection();
 
 int main(){
     int selection;
     int **matrice1 = NULL, **matrice2 = NULL;
-    int nbLignes=0, nbColonnes = 2;
+    int nbLignes=0, nbColonnes = 0;
+    int valeurMin = 0, valeurMax = 10;
+    char ncolonne[MAX];
+    char nligne[MAX];
+    pid_t execfils;
     FILE *mat1File;
     FILE *mat2File;
+    char *argAddition[6] = {"./somme", "matrice1.bin", "matrice2.bin",  NULL,  NULL, NULL};
+    char *argProduit[6] = {"./produit", "matrice1.bin", "matrice2.bin", NULL, NULL, NULL};
     MENU:
     affichageSelection();
     scanf("%d",&selection);
@@ -15,35 +27,104 @@ int main(){
         case 1:
             printf("Nombre de lignes : ");
             scanf("%d",&nbLignes);
-            matrice1 = creationMatrice(nbLignes);
+            printf("Nombre de colonne : ");
+            scanf("%d", &nbColonnes);
+            sprintf(nligne, "%d", nbLignes);
+            sprintf(ncolonne, "%d", nbColonnes);
+            argAddition[3] = nligne; argAddition[4] = ncolonne;
+            argProduit[3] = nligne; argProduit[4] = ncolonne;
+            matrice1 = creationMatrice(nbLignes, nbColonnes);
             mat1File = fopen("matrice1.bin", "wb");
-            fwrite(matrice1, sizeof(matrice1), 1, mat1File);
+            for (int i = 0; i < nbLignes; ++i) {
+                fwrite(matrice1[i], sizeof(int), nbColonnes, mat1File);
+            }
             fclose(mat1File);
+            free(matrice1);
             goto MENU;
         case 2:
             printf("Nombre de lignes : ");
             scanf("%d",&nbLignes);
-            matrice2 = creationMatrice(nbLignes);
+            printf("Nombre de colonne : ");
+            scanf("%d", &nbColonnes);
+            sprintf(nligne, "%d", nbLignes);
+            sprintf(ncolonne, "%d", nbColonnes);
+            argAddition[3] = nligne; argAddition[4] = ncolonne;
+            argProduit[3] = nligne; argProduit[4] = ncolonne;
+            matrice2 = creationMatrice(nbLignes, nbColonnes);
             mat2File = fopen("matrice2.bin", "wb");
-            fwrite(matrice2, sizeof(matrice2), 1, mat2File);
+            for (int i = 0; i < nbLignes; ++i) {
+                fwrite(matrice2[i], sizeof(int), nbColonnes, mat2File);
+            }
             fclose(mat2File);
+            free(matrice2);
             goto MENU;
         case 3:
+            printf("Nombre de lignes : ");
+            scanf("%d",&nbLignes);
+            printf("Nombre de colonne : ");
+            scanf("%d", &nbColonnes);
+            printf("Valeur Min : ");
+            scanf("%d",&valeurMin);
+            printf("Valeur Max : ");
+            scanf("%d", &valeurMax);
+            sprintf(nligne, "%d", nbLignes);
+            sprintf(ncolonne, "%d", nbColonnes);
+            argAddition[3] = nligne; argAddition[4] = ncolonne;
+            argProduit[3] = nligne; argProduit[4] = ncolonne;
+            matrice1 = generationMatriceAleatoire(nbLignes, nbColonnes, valeurMin, valeurMax);
+            mat1File = fopen("matrice1.bin", "wb");
+            for (int i = 0; i < nbLignes; ++i) {
+                fwrite(matrice1[i], sizeof(int), nbColonnes, mat1File);
+            }
+            fclose(mat1File);
+            free(matrice1);
+            matrice2 = generationMatriceAleatoire(nbLignes, nbColonnes, valeurMin, valeurMax);
+            mat2File = fopen("matrice2.bin", "wb");
+            for (int i = 0; i < nbLignes; ++i) {
+                fwrite(matrice2[i], sizeof(int), nbColonnes, mat2File);
+            }
+            fclose(mat2File);
+            free(matrice2);
+            goto MENU;
+        case 4:
             if (matrice1==NULL || matrice2==NULL){
                 printf("Vous devez d'abord creer vos matrice. \n");
             }else {
-                additionMatrice(matrice1, matrice2, nbLignes, nbColonnes);
+                execfils = fork();
+                if(execfils == 0) {
+                    //Addition
+                    printf("Addition des matrices : \n");
+                    execv(argAddition[0], argAddition);
+                } else{
+                    wait(0);
+                }
             }
             goto MENU;
         case 5:
-            if (matrice1 == NULL || matrice2 == NULL){
+            if (matrice1==NULL || matrice2==NULL){
                 printf("Vous devez d'abord creer vos matrice. \n");
             }else {
-                afficherMatrice(matrice1, nbLignes, nbColonnes);
-                afficherMatrice(matrice2, nbLignes, nbColonnes);
+                execfils = fork();
+                if(execfils == 0) {
+                    //Multiplication
+                    printf("Multiplication des matrices : \n");
+                    execv(argProduit[0], argProduit);
+                }else{
+                    wait(0);
+                }
             }
             goto MENU;
         case 6:
+            if (matrice1 == NULL || matrice2 == NULL){
+                printf("Vous devez d'abord creer vos matrice. \n");
+            }else {
+                printf("Matrice 1: \n");
+                afficherMatrice(matrice1, nbLignes, nbColonnes);
+                printf("Matrice 2 : \n");
+                afficherMatrice(matrice2, nbLignes, nbColonnes);
+            }
+            goto MENU;
+        case 7:
             break;
         default :
             printf("Erreur de selection. \n");
@@ -54,10 +135,11 @@ void affichageSelection(){
     printf("Outils de calcul de matrice : \n");
     printf("1 - Creer une matrice 1 \n");
     printf("2 - Creer une matrice 2 \n");
-    printf("3 - Additionner les 2 matrices \n");
-    printf("4 - Multiplier les 2 matrices \n");
-    printf("5 - Afficher les matrice \n");
-    printf("6 - Quitter \n");
+    printf("3 - Creer des matrices Aléatoires \n");
+    printf("4 - Additionner les 2 matrices \n");
+    printf("5 - Multiplier les 2 matrices \n");
+    printf("6 - Afficher les matrice \n");
+    printf("7 - Quitter \n");
     printf("Selection : ");
 };
 void afficherMatrice(int **matrice, int nbLignes, int nbColonnes){
@@ -68,18 +150,28 @@ void afficherMatrice(int **matrice, int nbLignes, int nbColonnes){
         printf("\n");
     }
 }
-int **creationMatrice(int nbLignes){
-    int nbColonne;
+int **creationMatrice(int nbLignes, int nbColonnes){
     int **matrice;
-    printf("Nombre de colonne : ");
-    scanf("%d", &nbColonne);
-    matrice = malloc(nbColonne * sizeof(int*));
-    for (int k = 0; k < nbColonne; k++) {
+    matrice = malloc(nbColonnes * sizeof(int*));
+    for (int k = 0; k < nbColonnes; k++) {
         matrice[k] = malloc(nbLignes * sizeof(int));
     }
     for (int i = 0; i < nbLignes; i++) {
-        for (int j = 0; j < nbColonne; j++) {
+        for (int j = 0; j < nbColonnes; j++) {
             scanf("%d", &matrice[i][j]);
+        }
+    }
+    return matrice;
+}
+int **generationMatriceAleatoire(int nbLignes, int nbColonnes, int valeurMin, int valeurMax){
+    int **matrice;
+    matrice = malloc(nbColonnes * sizeof(int*));
+    for (int k = 0; k < nbColonnes; k++) {
+        matrice[k] = malloc(nbLignes * sizeof(int));
+    }
+    for (int i = 0; i < nbLignes; i++) {
+        for (int j = 0; j < nbColonnes; j++) {
+            matrice[i][j] = (rand() % (valeurMax - valeurMin + 1)) + valeurMin;
         }
     }
     return matrice;
--- SR01/TP2/exercice3/produit.c
+++ SR01/TP2/exercice3/produit.c
@@ -1,4 +1,95 @@
-//
-// Created by Amaury SALMON on 17/12/2019.
-//
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+int main(int argc, char *argv[]){
+    if (argc < 5) {   // Test de la commande (verification du bon nombre de paramètre
+        printf("Usage : produit {matrice1.bin} {matrice2.bin} nbligne nbcolonne \n");
+    }else {
+        // Initialisation variables
+        int **somme;
+        int i=0,j;
+        FILE *mat1File;
+        FILE *mat2File;
+        int nbLignes = atoi(argv[3]);
+        int nbColonnes = atoi(argv[4]);
+        int matrice1[nbLignes][nbColonnes];
+        int matrice2[nbLignes][nbColonnes];
+        pid_t PID[nbLignes];
+        int pidPipe[nbLignes][2];
+        for (int numpid = 0; numpid < nbLignes; numpid++) {
+            pipe(pidPipe[numpid]);
+        }
+
+        //ouverture des fichier binaire
+        mat1File = fopen(argv[1], "rb");
+        mat2File = fopen(argv[2], "rb");
+
+        // On test si les fichiers ont bien été ouvert
+        if (mat1File == NULL){
+            printf("Impossible de lire le fichier matrice1\n");
+            exit(1);
+        }
+        if (mat2File == NULL){
+            printf("Impossible de lire le fichier matrice2\n");
+            exit(1);
+        }
+
+        // On attribu la mémoire suffisante à notre matrice résultat
+        somme = malloc(nbLignes * sizeof(int *));
+        for (int k = 0; k < nbLignes; k++) {
+            somme[k] = malloc(nbColonnes * sizeof(int));
+        }
+
+        // Lecture des fichiers binaires
+        fread(matrice1, nbLignes*sizeof(int), nbColonnes, mat1File);
+        fread(matrice2, nbLignes*sizeof(int), nbColonnes, mat2File);
+        // On les referme directement pour ne pas les laisser ouvert
+        fclose(mat1File);
+        fclose(mat2File);
+
+        // creation des processus fils pour les N lignes
+        for (j = 0; j <nbLignes; j++) {
+            PID[i] = fork();
+            if (PID[i] > 0) {
+                i++;
+            } else if (PID[i] == 0) {
+                break;
+            } else {
+                printf("Erreur de creation de processus \n");
+            }
+        }
+        if (i == nbLignes) {
+            i = nbLignes-1;
+        }
+
+        // Calcul des lignes pour les nbLignes fils
+        if (PID[i] == 0) {
+            for (int l = 0; l < nbColonnes; l++) {
+                for (int k = 0; k < nbLignes; k++) {
+                    somme[i][l] += matrice1[i][k] * matrice2[k][l];
+                }
+            }
+
+            close(pidPipe[i][0]);
+            write(pidPipe[i][1], somme[i], nbLignes * sizeof(int));
+        } else { // Attente du père pour le résultat et affichage de la matrice finale
+            wait(0);
+            for (int k = 0; k < nbLignes; k++) {
+                close(pidPipe[k][1]);
+                read(pidPipe[k][0], somme[k], nbLignes * sizeof(int));
+                for (int l = 0; l < nbColonnes; l++) {
+                    printf("| %d ", somme[k][l]);
+                }
+                printf("|\n");
+            }
+        }
+
+        // on n'oublie pas de libérer la mémoire réservé
+        free(somme);
+    }
+}
